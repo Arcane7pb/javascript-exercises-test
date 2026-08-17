8438f6e99663ffe7dbed8aa3f300aec102c899a9
Arcane <291515644+Arcane7pb@users.noreply.github.com>
2026-08-17 19:44:46 +0200

    Solves ex 6

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeat) {
+    if (repeat < 0) {return "ERROR"};
+    let stringRepeated ="";
+    for (i = 1; i <= repeat ; i++){
+        stringRepeated += string;
+    };
+    return stringRepeated
 };
 
 // Do not edit below this line
