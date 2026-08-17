bcb035163510e13a2fc96f9451e061bcf1962cd5
Arcane <291515644+Arcane7pb@users.noreply.github.com>
2026-08-17 21:02:32 +0200

    Solves ex 9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if( a < 1 | b < 1) return "ERROR";
+    if(!Number.isInteger(a) | !Number.isInteger(b)) return "ERROR";
+    let sum = 0;
+    
+    if(a > b){
+        let pivot = a;
+        a = b
+        b = pivot
+    }
 
+    for (i = a; i <= b; i++){
+        sum += i
+    }
+    return sum
 };
 
 // Do not edit below this line
