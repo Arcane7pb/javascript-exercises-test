c8afe877aee21564815106933452fe7a05104da6
Arcane <291515644+Arcane7pb@users.noreply.github.com>
2026-08-17 20:26:57 +0200

    Solces ex 8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    const arg = Array.prototype.slice.call(arguments, 1);
+    for (word of arg){
+        while (arr.find((search) => search === word)){
+            const index = arr.findIndex((search) => search === word);
+            arr.splice(index,1);
+        }
+    }
+    return arr
 };
 
 // Do not edit below this line
